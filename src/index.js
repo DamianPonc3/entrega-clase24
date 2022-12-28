62e120a4bf3e73a6b4af4f61bdade4174d12700c
Damian Ponce <damianemponce@gmail.com>
2022-12-28 08:02:44 -0300

    entrega

--- src/index.js
+++ src/index.js
@@ -1,44 +1,26 @@
 import express from "express";
-import mainRouter from "./routes/user.routes.js";
-import session from "express-session";
-import passport from "passport";
-import { loginFunc, signUpFunc } from "./services/auth.js";
-import MongoStore from "connect-mongo";
+import infoRouter from "./routes/info.route.js";
+import randomRouter from "./routes/random.route.js";
 import Config from "./config/index.js";
-import { initDb } from "./db/db.js";
+import minimist from "minimist";
+
+const optionalArgsObject = {
+  alias: {
+    p: "puerto",
+  },
+  default: {
+    p: "8080",
+  },
+};
+
+export const args = minimist(process.argv, optionalArgsObject);
 
 const app = express();
 
 app.use(express.json());
 
-await initDb();
-console.log("Conectado a la DB!");
+app.use("/api", infoRouter, randomRouter);
 
-const ttlSeconds = 600;
-
-const StoreOptions = {
-  store: MongoStore.create({
-    mongoUrl: Config.MONGO_ATLAS_URL,
-  }),
-  secret: "secretString",
-  resave: true,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: ttlSeconds * 1000,
-  },
-};
-
-app.use(session(StoreOptions));
-
-app.use(passport.initialize());
-
-app.use(passport.session());
-
-passport.use("login", loginFunc);
-passport.use("signup", signUpFunc);
-
-app.use("/api", mainRouter);
-
-app.listen(Config.PUERTO, () =>
-  console.log(`Escuchando en el puerto ${Config.PUERTO}`)
+app.listen(Config.PORT, () =>
+  console.log(`Escuchando en el puerto ${Config.PORT}`)
 );
